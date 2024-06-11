c3f94dde7405d8b898f716738129bcff58346479
Chidera Mosanya <deramosanya@gmail.com>
2024-06-11 21:05:06 +0100

    initial code commit

---
+++ src/main/java/ai/spaarkl/co/recommender/batchinference/BatchinferenceApplication.java
@@ -0,0 +1,13 @@
+package ai.spaarkl.co.recommender.batchinference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchinferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchinferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/spaarkl/co/recommender/batchinference/BatchinferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.spaarkl.co.recommender.batchinference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchinferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
